7fe46a9041b8c17f67c162558e2f793fabb5708c
José Carlos <jc.esteban.gomez@gmail.com>
2017-10-02 21:23:15 +0200

    Limpiamos el AppComponent y llevamos el listado y formulario de contactos a los componentes de ruta correspondientes. Añadimos barra de navegación.

--- src/app/ruta-a/ruta-a.component.ts
+++ src/app/ruta-a/ruta-a.component.ts
@@ -1,4 +1,6 @@
+import { ContactosService } from '../contactos.service';
 import { Component, OnInit } from '@angular/core';
+import { Contacto } from '../contacto';
 
 @Component({
   selector: 'app-ruta-a',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ruta-a.component.css']
 })
 export class RutaAComponent implements OnInit {
+  nombres: Contacto[];
 
-  constructor() { }
+  // Para hacer la inyección de dependencias de un servicio
+  // debemos hacerlo en el constructor de la clase. Anotamos
+  // un parámetro con el tipo de servicio a inyectar y
+  // añadimos el modificador de acceso correspondiente al
+  // parámetro.
+  constructor(private _contactosService: ContactosService) { }
 
-  ngOnInit() {
+  // El hook 'OnInit' se ejecuta cuando el componente tiene
+  // asociado su template correspondiente, por tanto es el
+  // momento ideal para enlazar datos entre ellos.
+  ngOnInit(): void {
+    this.nombres = this._contactosService.obternerContactos();
   }
 
+  eliminarContacto(nombre: Contacto): void{
+    this._contactosService.eliminarContacto(nombre);
+    this.nombres= this._contactosService.obternerContactos();
+  }
 }
--- src/app/ruta-b/ruta-b.component.ts
+++ src/app/ruta-b/ruta-b.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ContactosService } from '../contactos.service';
+import { Contacto } from '../contacto';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ruta-b',
   templateUrl: './ruta-b.component.html',
   styleUrls: ['./ruta-b.component.css']
 })
-export class RutaBComponent implements OnInit {
+export class RutaBComponent {
 
-  constructor() { }
+  constructor(
+    private _contactosService: ContactosService,
+    private _router: Router) { }
 
-  ngOnInit() {
+  crearContacto(contacto: Contacto): void{
+    this._contactosService.agregarContacto(contacto);
+    this._router.navigate(['/lista-contactos'])
   }
 
 }
